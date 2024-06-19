d68a1a17c8cd2803c89c4b8f2e946214f43fcf90
OmarAlraisi <o.r.raisi@gmail.com>
2024-06-19 23:39:30 +0400

    handles only tcp connections and ignores everything else
    still doesnt do shit with it, only print source and destination IPs and Ports

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cargo b --release
 sudo setcap cap_net_admin=eip ./target/release/tcp
 ./target/release/tcp &
 pid=$!
-sudo ip a add 192.168.100.254/24 dev tun0
+sudo ip a add 192.168.1.1/24 dev tun0
 sudo ip link set up dev tun0
 trap "kill $pid" INT TERM
 wait $pid
