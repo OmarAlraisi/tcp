c980c3524b507696ea834238358a3bbee77930fe
OmarAlraisi <o.r.raisi@gmail.com>
2024-06-19 19:37:50 +0400

    starts intercepting connections
    It intercepts packets using the Linux TUN/Tap (TUN) and ignores anything that is not IPv4

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cargo b --release
+sudo setcap cap_net_admin=eip ./target/release/tcp
+./target/release/tcp &
+pid=$!
+sudo ip a add 192.168.100.254/24 dev tun0
+sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
+wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tun_tap::{Iface, Mode};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = Iface::new("tun0", Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+
+    loop{
+        let len = nic.recv(&mut buf)?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+
+        if proto != 0x0800 {
+            continue;
+        }
+
+        println!("Received {} bytes (flags: {:x}, proto: {:x}): {:?}", len - 4, flags, proto, &buf[4..len]);
+    }
 }
