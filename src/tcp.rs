eec35fc592ab7a41216396b6d6f2358044ba9ea2
OmarAlraisi <o.r.raisi@gmail.com>
2024-06-20 22:02:23 +0400

    adds send and receive sequence spaces

--- src/tcp.rs
+++ src/tcp.rs
@@ -11,19 +11,103 @@ pub enum State {
 
 pub struct Connection {
     state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
+}
+
+/// State of the Send Sequence Space. (RFC 9293 - Section 3.3.1 - Figure 3)
+///
+/// ```
+///             1         2          3          4
+///        ----------|----------|----------|----------
+///               SND.UNA    SND.NXT    SND.UNA
+///                                    +SND.WND
+///
+///  1 - old sequence numbers that have been acknowledged
+///  2 - sequence numbers of unacknowledged data
+///  3 - sequence numbers allowed for new data transmission
+///  4 - future sequence numbers that are not yet allowed
+/// ```
+struct SendSequenceSpace {
+    /// send unacknowledged
+    una: usize,
+    /// send next
+    nxt: usize,
+    /// send window
+    wnd: usize,
+    /// send urgent pointer
+    up: bool,
+    /// segment sequence number used for last window update
+    wl1: usize,
+    /// segment acknowledgment number used for last window update
+    wl2: usize,
+    /// initial send sequence number
+    iss: usize,
+}
+impl Default for SendSequenceSpace {
+    fn default() -> Self {
+        Self {
+            una: Default::default(),
+            nxt: Default::default(),
+            wnd: Default::default(),
+            up: Default::default(),
+            wl1: Default::default(),
+            wl2: Default::default(),
+            iss: Default::default(),
+        }
+    }
+}
+
+// For example if a segment has a SEG.SEQ of 1234 and its data has a length of 1250, then the next segment
+// should have a SEG.SEQ of 2483
+
+/// State of the Receive Sequence Space. (RFC 9293 - Section 3.3.1 - Figure 4)
+///
+/// ```
+///    1          2          3
+///             ----------|----------|----------
+///                    RCV.NXT    RCV.NXT
+///                              +RCV.WND
+///
+///  1 - old sequence numbers that have been acknowledged
+///  2 - sequence numbers allowed for new reception
+///  3 - future sequence numbers that are not yet allowed
+/// ```
+struct RecvSequenceSpace {
+    /// receive next
+    nxt: u32,
+    /// receive window
+    wnd: u16,
+    /// receive urgent pointer
+    up: bool,
+    /// initial receive sequence number
+    irs: u32,
+}
+
+impl Default for RecvSequenceSpace {
+    fn default() -> Self {
+        Self {
+            nxt: Default::default(),
+            wnd: Default::default(),
+            up: Default::default(),
+            irs: Default::default(),
+        }
+    }
 }
 
 impl Default for Connection {
     fn default() -> Self {
         Connection {
             state: State::Listen,
+            send: Default::default(),
+            recv: Default::default(),
         }
     }
 }
 
 impl Connection {
     pub fn on_packet<'a>(
-        &self,
+        &mut self,
         nic: &mut Iface,
         iphdr: &'a Ipv4HeaderSlice,
         tcphdr: &'a TcpHeaderSlice,
@@ -37,16 +121,19 @@ impl Connection {
                     return Ok(());
                 }
 
+                // Keep track of sender info
+                self.recv.irs = tcphdr.sequence_number();
+                self.recv.nxt = tcphdr.sequence_number() + 1;
+                self.recv.wnd = tcphdr.window_size();
+
                 // Create a packet to conitnue establishing the connection
-                let sequence_number = 0u32;
-                let window_size = 0u16;
                 let mut syn_ack_tcp = TcpHeader::new(
                     tcphdr.destination_port(),
                     tcphdr.source_port(),
-                    sequence_number,
-                    window_size,
+                    0,
+                    10,
                 );
-
+                syn_ack_tcp.acknowledgment_number = tcphdr.sequence_number() + 1;
                 syn_ack_tcp.syn = true;
                 syn_ack_tcp.ack = true;
 
